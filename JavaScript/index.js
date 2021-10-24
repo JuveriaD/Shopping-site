e724a3a83d40ad38606da506dd51c0ef9f5262e3
juveria dalvi <juveriadalvi4@gmail.com>
2021-10-24 17:51:34 +0530

    added JS folder

---
+++ JavaScript/index.js
@@ -0,0 +1,40 @@
+// variables in javascript;
+var a = 23;
+var b = 'juveria';
+console.log(a + b);
+
+// Operators in JavaScript
+// Operand - entities on which operators operate
+// In 3 + 4 '+' is the operator and 3,4 are operand
+// 1. unary operator - It has single operand ( x = - x)
+// EXAMPLE OF UNARY OPERATOR
+// a = -a;
+// console.log(a);
+// 2. binary operator -  It has two operand ( x = x + 2)
+// EXAMPLE OF BINARY OPERATOR
+// a = 3 + 5;
+// console.log(a);
+
+var num1 = 2;
+var num2 = 3;
+
+// ARITHEMATIC OPERATORS in action in JavaScript
+// 1. addition
+// 2. subtraction
+// 3. multiplication
+// 4. division
+// 5. module
+// 6. to the power(raised to/ exponential operator)
+// 7. increment
+// 8. decrement
+
+// console.log("The  value of num1 + num2 is " + (num1 + num2));
+// console.log("The  value of num1 - num2 is " + (num1 - num2));
+// console.log("The  value of num1 * num2 is " + (num1 * num2));
+// console.log("The  value of num1 / num2 is " + (num1 / num2));
+// console.log("The  value of num1 % num2 is " + (num1 % num2));
+// console.log("The  value of num1 ** num2 is " + (num1 ** num2));
+// console.log("The  value of num1 ++ is " +( num1++));
+// console.log("The  value of num1 -- is " + (num1--));
+// console.log("The  value of ++num1  is " + (++num1));
+// console.log("The  value of --num1  is " + (--num1));
